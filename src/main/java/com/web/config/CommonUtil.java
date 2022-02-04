d5ba80f60569630a6883f830882f39e9ac11d5da
taetanee <xpxksl@gmail.com>
2022-02-04 23:15:36 +0900

    CommonUtil의 날짜 가져오는 함수 만듦

--- src/main/java/com/web/config/CommonUtil.java
+++ src/main/java/com/web/config/CommonUtil.java
@@ -1,11 +1,28 @@
 package com.web.config;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class CommonUtil {
+
+    public static void main(String args[]){
+        CommonUtil _this = new CommonUtil();
+        String result1 = _this.getDate("yyyyMMdd");
+        System.out.println(result1);
+    }
+
     public static boolean isEmptyOrNull(String str) {
         if (str != null && !str.isEmpty()) {
             return false;
         } else {
             return true;
         }
+    }
+
+    public static String getDate(String pattern){
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        Calendar c1 = Calendar.getInstance();
+        String strToday = sdf.format(c1.getTime());
+        return strToday;
     }
 }
