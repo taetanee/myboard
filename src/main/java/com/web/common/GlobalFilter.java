e06e6c03de662b4772b46d00f5371b3d6670cd7e
taetanee <xpxksl@gmail.com>
2022-03-27 21:26:17 +0900

    Filter 초안 작업

--- src/main/java/com/web/common/GlobalFilter.java
+++ src/main/java/com/web/common/GlobalFilter.java
@@ -3,11 +3,15 @@ package com.web.common;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import java.io.IOException;
 
 @Component
 public class GlobalFilter implements Filter {
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         //TODO : 필터 전처리 작업필요할 시 넣자
+        chain.doFilter(request, response);
     }
+
 }
