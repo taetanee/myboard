96047cf5fd3564e867c56f1150fac4056375c7c1
taetanee <xpxksl@gmail.com>
2022-06-16 22:41:57 +0900

    코딩테스트 공부용 샘플 소스 작업

---
+++ src/main/java/com/web/dev/Solution0Sample.java
@@ -0,0 +1,21 @@
+package com.web.dev;
+
+
+/*
+문제제에에목
+https://programmers.co.kr/learn/courses/30/lessons/xxxxx
+ */
+
+public class Solution0Sample {
+    public static void main(String[] args) {
+
+        Solution0Sample s1 = new Solution0Sample();
+        System.out.println(s1.solution());
+        Solution0Sample s2 = new Solution0Sample();
+        System.out.println(s2.solution());
+    }
+    public int solution() {
+        int answer = 0;
+        return answer;
+    }
+}
