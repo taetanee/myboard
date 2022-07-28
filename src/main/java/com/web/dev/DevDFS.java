690172809ef442a886f329b6789c6c73706e2cb5
taetanee <xpxksl@gmail.com>
2022-07-28 21:53:00 +0900

    DevDFS 조건문 변경(기능변경없음)

--- src/main/java/com/web/dev/DevDFS.java
+++ src/main/java/com/web/dev/DevDFS.java
@@ -24,7 +24,7 @@ public class DevDFS {
         }
 
         for (int i = 0; i < n; i++) {
-            if (visited[i] != true) {
+            if (visited[i] == false) {
                 visited[i] = true;
                 output[depth] = arr[i];
                 perm(arr, output, visited, depth + 1, n, r);
