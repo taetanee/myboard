d0694ba1fd5f564aabaa443d5f1cb30692b2bc2b
taetanee <xpxksl@gmail.com>
2022-07-31 22:31:17 +0900

    코테공부 86491 할 예정

---
+++ src/main/java/com/web/solution/Solution86491.java
@@ -0,0 +1,18 @@
+package com.web.solution;
+
+/*
+문제제에에목
+https://school.programmers.co.kr/learn/courses/30/lessons/86491
+ */
+
+public class Solution86491 {
+    public static void main(String[] args) {
+
+        Solution86491 s1 = new Solution86491();
+        System.out.println(s1.solution(new int[][] {{60, 50}, {30, 70}, {60, 30}, {80, 40}}));
+    }
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        return answer;
+    }
+}
