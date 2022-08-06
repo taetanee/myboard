1c9bb077101aa00496b0c0f0ff3b8def0857e4ef
taetanee <xpxksl@gmail.com>
2022-08-06 16:13:12 +0900

    코테 86491

--- src/main/java/com/web/solution/Solution86491.java
+++ src/main/java/com/web/solution/Solution86491.java
@@ -1,18 +1,23 @@
 package com.web.solution;
 
 /*
-문제제에에목
-https://school.programmers.co.kr/learn/courses/30/lessons/86491
+최소직사각형
+https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=java
+https://jongwoon.tistory.com/79 : 참고해서 풀었음
  */
 
 public class Solution86491 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Solution86491 s1 = new Solution86491();
-        System.out.println(s1.solution(new int[][] {{60, 50}, {30, 70}, {60, 30}, {80, 40}}));
+        s1.solution(new int[][] {{60, 50}, {30, 70}, {60, 30}, {80, 40}});
     }
     public int solution(int[][] sizes) {
-        int answer = 0;
-        return answer;
+        int maxWidth = -1, maxHeight = -1;
+        for(int[] size : sizes) {
+            maxWidth = Math.max(Math.min(size[0], size[1]), maxWidth);
+            maxHeight = Math.max(Math.max(size[0], size[1]), maxHeight);
+        }
+        return maxWidth * maxHeight;
     }
 }
