b7a166569dba3bc84ae71770538f511348f80ef5
taetanee <xpxksl@gmail.com>
2022-08-07 22:51:49 +0900

    코테 60057 (하다가 잘못해서 새로 해야함)

---
+++ src/main/java/com/web/solution/Solution60057.java
@@ -0,0 +1,27 @@
+package com.web.solution;
+
+/*
+문자열 압축
+https://school.programmers.co.kr/learn/courses/30/lessons/60057
+ */
+
+import java.util.ArrayList;
+
+public class Solution60057 {
+
+    public static void main(String[] args) {
+        Solution60057 s1 = new Solution60057();
+        s1.solution("aabbaccc");//7
+//        s1.solution("ababcdcdababcdcd");//9
+//        s1.solution("abcabcdede");//8
+//        s1.solution("abcabcabcabcdededededede");//14
+//        s1.solution("xababcdcdababcdcd");//17
+    }
+
+    public int solution(String s) {
+
+        //TODO : 해야함
+        int answer = 0;
+        return answer;
+    }
+}
