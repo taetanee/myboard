1d01cdd332cf04f91c55a55421056e6f6556098f
taetanee <xpxksl@gmail.com>
2022-08-21 16:04:51 +0900

    샘플 소스 코드 수정

--- src/main/java/com/web/solution/Solution0Sample.java
+++ src/main/java/com/web/solution/Solution0Sample.java
@@ -10,6 +10,9 @@ public class Solution0Sample {
 
         Solution0Sample s1 = new Solution0Sample();
         System.out.println(s1.solution());
+
+        Solution0Sample s2 = new Solution0Sample();
+        System.out.println(s2.solution());
     }
     public int solution() {
         int answer = 0;
