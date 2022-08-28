2c99fd6e62cf317c000e6c1b89dcb57ec8470b8e
taetanee <xpxksl@gmail.com>
2022-08-28 23:22:42 +0900

    Enonioc 코테공부(초안)

---
+++ src/main/java/com/web/solution/SolutionEnonioc_2.java
@@ -0,0 +1,20 @@
+package com.web.solution;
+
+import java.util.*;
+
+public class SolutionEnonioc_2 {
+
+    public static void main(String[] args) {
+        SolutionEnonioc_2 s1 = new SolutionEnonioc_2();
+        s1.solution(new int[][] {{1,0,5},{2,2,2},{3,3,1},{4,4,1},{5,10,2}}); //1,3,4,2,5
+
+        //SolutionEnonioc_2 s2 = new SolutionEnonioc_2();
+        //s2.solution(new int[][] {{1,0,2},{2,1,3},{3,3,2},{4,9,1},{5,10,2}}); //1,3,2,4,5
+    }
+    public int[] solution(int[][] data){
+        int[] answer = {};
+
+        return answer;
+    }
+
+}
