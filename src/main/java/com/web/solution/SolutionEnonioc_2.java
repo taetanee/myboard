4c7ac38c23686f20fb11e97b03e15c4be5cd6bf9
taetanee <xpxksl@gmail.com>
2022-09-10 23:37:32 +0900

    보류....

--- src/main/java/com/web/solution/SolutionEnonioc_2.java
+++ src/main/java/com/web/solution/SolutionEnonioc_2.java
@@ -12,8 +12,27 @@ public class SolutionEnonioc_2 {
         //s2.solution(new int[][] {{1,0,2},{2,1,3},{3,3,2},{4,9,1},{5,10,2}}); //1,3,2,4,5
     }
     public int[] solution(int[][] data){
-        int[] answer = {};
+        ArrayList<Integer> tempAnswer = new ArrayList();
 
+        Queue<HashMap<Integer,Integer>> queue = new LinkedList<>();
+        HashMap<Integer,Integer> current = new HashMap();
+
+        int time = 0;
+        while( tempAnswer.size() != data.length ){
+            //[시작] 시간마다 요청 찾기
+            for(int i=0; i<data.length; i++){
+                if( data[i][1] == time ){
+                    HashMap<Integer,Integer> temp = new HashMap();
+                    temp.put(time, data[i][0]);
+                    queue.add(temp);
+                }
+            }
+            //[종료] 시간마다 요청 찾기
+
+            time++;
+        }
+
+        int[] answer = {};
         return answer;
     }
 
