08be26cb57c4d3ce49419ea700bd2f1ec96f8f3f
taetanee <xpxksl@gmail.com>
2022-10-02 02:46:34 +0900

    인터셉터 작업, 필요시 작업하면 됨

---
+++ src/main/java/com/web/common/GlobalInterceptor.java
@@ -0,0 +1,24 @@
+package com.web.common;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Component
+public class GlobalInterceptor  extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //TODO : 필요시 작업
+        return true;
+    }
+
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //TODO : 필요시 작업
+        System.out.println("afterCompletion");
+    }
+}
---
+++ src/main/java/com/web/common/WebMvcConfig.java
@@ -0,0 +1,48 @@
+package com.web.common;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private GlobalInterceptor globalInterceptor;
+
+    //로그인 체크 페턴
+    private String[] includedUrlPatterns = {
+            "/mypage/**"
+    };
+
+    //로그인 체크 예외
+    private String[] excludedUrlPatterns = {
+            //필요시 추가
+    };
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(globalInterceptor)
+                .addPathPatterns(Arrays.asList(includedUrlPatterns))
+                .excludePathPatterns(Arrays.asList(excludedUrlPatterns))
+        ;
+    }
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/resources/"
+            , "classpath:/static/"
+    };
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+}
