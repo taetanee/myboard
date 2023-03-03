60b356a04f14b2c2f94c800f59647c0992b6c450
taetanee <xpxksl@gmail.com>
2023-03-04 03:43:20 +0900

    프로미스로 전환

--- src/main/resources/static/common.js
+++ src/main/resources/static/common.js
@@ -3,30 +3,29 @@
  */
 var Common = {
     ajaxTransaction : function ( _param ) {
-
-        if(this.isNull(_param)){
-            this.error("param is null");
-        }
-
-        if(this.isNull(_param["url"])){
-            this.error('url is null');
-        }
-
-        var param = _param;
-        var result = undefined;
-
-        $.ajax({
-            url : param["url"]
-            , type : "POST"
-            , async: false
-            , success : function (ajaxResult) {
-                result = ajaxResult;
+        return new Promise(function (resolve, reject) {
+            if(Common.isNull(_param)){
+                Common.error("param is null");
             }
-            , error : function () {
-                alert("error 발생");
+
+            if(Common.isNull(_param.url)){
+                Common.error('url is null');
             }
+
+            var reqUrl = _param.url
+
+            $.ajax({
+                url : reqUrl
+                , type : "POST"
+                , async: false
+                , success : function (resData) {
+                    resolve(resData);
+                }
+                , error       : function(xhr, errorName, error) {
+                    reject(xhr);
+                }
+            });
         });
-        return result;
     }
     , isNull : function ( _param ) {
         if( _param === undefined || _param === null) {
