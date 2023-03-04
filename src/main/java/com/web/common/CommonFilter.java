eb5d8832ce8eec9d693ab07adf08911901d023c0
taetanee <xpxksl@gmail.com>
2023-03-05 02:34:38 +0900

    Filter에서 OncePerRequestFilter상속 받도록 수정

--- src/main/java/com/web/common/CommonFilter.java
+++ src/main/java/com/web/common/CommonFilter.java
@@ -1,15 +1,18 @@
 package com.web.common;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class CommonFilter implements Filter {
+public class CommonFilter extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         //필요시 작업
         chain.doFilter(request, response);
     }
