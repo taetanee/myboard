164ce1d62b673af204bdb10fdea0a4bf4c8a319d
taetanee <xpxksl@gmail.com>
2023-04-12 22:07:29 +0900

    exceptionTest 테스트 코드 추가

--- src/main/java/com/web/service/WeatherService.java
+++ src/main/java/com/web/service/WeatherService.java
@@ -1,14 +1,11 @@
 package com.web.service;
 
 
-import com.web.common.CommonException;
 import com.web.common.DataGoAPI;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.rmi.server.ExportException;
 import java.util.HashMap;
 
 @Slf4j
@@ -20,10 +17,6 @@ public class WeatherService {
 
 
 	public HashMap<Object,Object> getShortTermWeather(HashMap<String,String> param) throws Exception{
-
-		if( false ){
-			throw new CommonException(CommonException.MD_ERR_EXCEPTION);
-		}
 
 		HashMap<Object,Object> result = new HashMap<>();
 		result = dataGoAPI.getShortTermWeather(param);
