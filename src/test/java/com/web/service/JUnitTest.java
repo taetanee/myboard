f2aac9977cd55e5dc1a56d9674a936281dee6727
taetanee <xpxksl@gmail.com>
2025-02-15 16:24:57 +0900

    setDetails 자바스크립트 함수 추가 및 랜덤단어 가져오기 기능 추가

--- src/test/java/com/web/service/JUnitTest.java
+++ src/test/java/com/web/service/JUnitTest.java
@@ -1,5 +1,6 @@
 package com.web.service;
 
+import com.web.common.util.CommonUtil;
 import com.web.dto.CovidDTO;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -19,61 +20,70 @@ public class JUnitTest {
     @InjectMocks
     private CommonService commonService;
 
+    @InjectMocks
+    private CommonUtil commonUtil;
+
     @Before
     public void setup(){
         MockitoAnnotations.initMocks(this);
     }
 
+
+//    @Test
+//    public void test1() {
+//        try {
+//            //given
+//            double x1 = 3.3;
+//
+//            //when
+//            double x2 = 3.5;
+//            CovidDTO y2 = commonService.junitTest();
+//
+//            //then
+//            assertThat(x1, closeTo(x2,0.3));
+//
+//            assertThat(y2.getUpdateDt(), is("A"));
+//        } catch (Exception e){
+//            System.out.println(e);
+//        }
+//    }
+
+    //@Test
+//    public void test2() {
+//        try {
+//            //given
+//            double x1 = 3.3;
+//
+//            //when
+//            double x2 = 3.5;
+//            CovidDTO y2 = commonService.junitTest();
+//
+//            //then
+//            assertThat(x1, closeTo(x2,0.1)); //이 경우에는 error 발생
+//        } catch (Exception e){
+//            System.out.println(e);
+//        }
+//    }
+
+    //@Test
+//    public void test3() {
+//        try {
+//            //given
+//            double x1 = 3.3;
+//
+//            //when
+//            double x2 = 3.5;
+//            CovidDTO y2 = commonService.junitTest();
+//
+//            //then
+//            assertThat(y2.getUpdateDt(), is("B")); //이 경우에는 error 발생
+//        } catch (Exception e){
+//            System.out.println(e);
+//        }
+//    }
+
     @Test
     public void test1() {
-        try {
-            //given
-            double x1 = 3.3;
-
-            //when
-            double x2 = 3.5;
-            CovidDTO y2 = commonService.junitTest();
-
-            //then
-            assertThat(x1, closeTo(x2,0.3));
-
-            assertThat(y2.getUpdateDt(), is("A"));
-        } catch (Exception e){
-            System.out.println(e);
-        }
-    }
-
-    //@Test
-    public void test2() {
-        try {
-            //given
-            double x1 = 3.3;
-
-            //when
-            double x2 = 3.5;
-            CovidDTO y2 = commonService.junitTest();
-
-            //then
-            assertThat(x1, closeTo(x2,0.1)); //이 경우에는 error 발생
-        } catch (Exception e){
-            System.out.println(e);
-        }
-    }
-
-    //@Test
-    public void test3() {
-        try {
-            //given
-            double x1 = 3.3;
-
-            //when
-            double x2 = 3.5;
-            CovidDTO y2 = commonService.junitTest();
-
-            //then
-            assertThat(y2.getUpdateDt(), is("B")); //이 경우에는 error 발생
-        } catch (Exception e){
-            System.out.println(e);
-        }
+        System.out.println(commonUtil.getUUID(3));
     }
 }
