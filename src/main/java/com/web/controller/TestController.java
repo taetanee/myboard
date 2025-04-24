2bb8ae26cc6a30b5cfa4d2fb1dd7f4f01afadd57
taetanee <xpxksl@gmail.com>
2025-04-25 06:45:20 +0900

    리액트에서 데이터 가져오록 변경(spring-security , test컨트롤러 추가)

---
+++ src/main/java/com/web/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.web.controller;
+
+import com.web.common.CommonResponse;
+import com.web.service.CommonService;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@Api(tags="테스트 컨트롤러")
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+	@Autowired
+	private CommonService commonService;
+
+	@GetMapping("/getUuid")
+	public ResponseEntity<?> getUuid(){
+		CommonResponse response = new CommonResponse();
+		HashMap<String,Object> result = commonService.getUuid();
+		response.setResult(result);
+		return ResponseEntity.ok(response);
+	}
+}
