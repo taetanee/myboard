229528570da14c157d81d780138ae437d8d0eec9
taetanee <xpxksl@gmail.com>
2026-02-23 04:16:22 +0900

    setAllowedOrigins 정리

--- src/main/java/com/web/common/config/SecurityConfig.java
+++ src/main/java/com/web/common/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("http://localhost","http://124.53.139.229","http://mypad.kr"));
+		config.setAllowedOrigins(Arrays.asList("https://mypad.kr:23000"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 		config.setAllowedHeaders(Arrays.asList("*"));
 		config.setAllowCredentials(true);
